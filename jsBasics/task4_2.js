1de8914f54f0197d1b048116a679751538abd455
Ivan Khaliavka <ivan.khaliavka@appflame.com>
2026-04-28 18:51:48 +0300

    new tasks

---
+++ jsBasics/task4_2.js
@@ -0,0 +1,7 @@
+let length = 10;
+let width = 5;
+let area = length * width;
+
+console.log("Довжина:", length);
+console.log("Ширина:", width);
+console.log("Площа прямокутника:", area);
---
+++ jsBasics/task4_3.js
@@ -0,0 +1,10 @@
+let radius = 5.5;
+let height = 12.8;
+let volume = Math.PI * radius ** 2 * height;
+let radiusRounded = radius.toFixed(2);
+let heightRounded = height.toFixed(2);
+let volumeRounded = volume.toFixed(2);
+
+console.log("Радіус:", radiusRounded);
+console.log("Висота:", heightRounded);
+console.log("Об'єм циліндра:", volumeRounded);
